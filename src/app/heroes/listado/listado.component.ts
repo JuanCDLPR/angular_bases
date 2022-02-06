aa27e0d3acad8330855ae9283e6c8ee0e7ca1f69
Juan De La Paz <JuanCDLPR@Outlook.com>
2022-02-06 15:15:35 -0600

    47. Directiva *ngIf

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -5,5 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './listado.component.html',
 })
 export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'goku', 'ironman', 'hulk'];
+  borrado: string = '';
+  status: boolean = false;
   constructor() {}
+
+  BorrarHeroe() {
+    this.borrado = this.heroes.shift() || '';
+    if (this.borrado == '') {
+      this.status = false;
+    } else {
+      this.status = true;
+    }
+    console.log(this.borrado);
+  }
 }

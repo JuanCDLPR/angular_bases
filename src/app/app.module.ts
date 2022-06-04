2e45b423297f6396acd5caaee8eab4850d353054
Juan De La Paz <JuanCDLPR@Outlook.com>
2022-06-04 16:46:52 -0500

    Módulo DBZ (Dragon Ball Z)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent, ContadorComponent],
-  imports: [BrowserModule, HeroesModule],
+  imports: [BrowserModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
